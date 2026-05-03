e6cbd98fe33acaf7c70f3d6c2e1fa63bbdcc2f1b
Wing <114337707+wing-0902@users.noreply.github.com>
2026-05-03 02:40:08 +0000

    nuxt create

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/image', '@unocss/nuxt'],
+  unocss: {
+    nuxtLayers: true,
+  }
+})
