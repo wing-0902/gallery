b35f914834f610f3e9edd568d200b3e385ca5a94
Wing <114337707+wing-0902@users.noreply.github.com>
2026-05-03 04:42:39 +0000

    いいね

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,4 +1,11 @@
 <template>
+  <header w-full flex justify-between items-end m-0>
+    <h1 m-0>Photo by Wing</h1>
+    <div flex m-x-0 m-y-2 gap-x-2>
+      <a i-hugeicons-home-02 href='https://wing.osaka' target='_blank'>Wing</a>
+      <a i-hugeicons-bluesky href='https://bsky.app/profile/wing.osaka' target='_blank'>bsky</a>
+    </div>
+  </header>
   <div>
     <slot />
   </div>
