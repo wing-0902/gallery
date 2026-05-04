1c08a15f42d5ea0f64ee08e8915c0b9a4ceca75e
Wing <114337707+wing-0902@users.noreply.github.com>
2026-05-04 23:02:46 +0000

    画像のサイズを変更 [skip ci]

--- app/components/GalleryList.vue
+++ app/components/GalleryList.vue
@@ -20,7 +20,7 @@ import photoList from '~/assets/list.json';
 <style scoped lang="scss">
 .grid-layout {
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(min(100%, 150px), 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(min(calc(50% - 16px), 250px), 1fr));
   gap: 16px;
 }
 </style>
