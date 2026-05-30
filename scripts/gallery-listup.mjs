5410eeef4bd7814bd789afd390d6630b6f738bf9
Wing <114337707+wing-0902@users.noreply.github.com>
2026-05-30 05:16:57 +0000

    順番変えた

--- scripts/gallery-listup.mjs
+++ scripts/gallery-listup.mjs
@@ -4,21 +4,18 @@ import path from 'path';
 const GALLERY_DIR = './public/gallery';
 const OUTPUT_JSON = './app/assets/list.json';
 
-// 対応する拡張子
 const EXTENSIONS = /\.(jpg|jpeg|png|webp|avif)$/i;
 
 try {
-  // ファイル一覧を取得
   const files = fs.readdirSync(GALLERY_DIR);
 
-  // 画像ファイルだけに絞り込み，ファイル名でソート
   const photoList = files
     .filter((file) => EXTENSIONS.test(file))
     .sort((b, a) =>
       a.localeCompare(b, undefined, { numeric: true, sensitivity: 'base' })
     );
 
-  // JSONに書き出し
+  // export JSON
   fs.writeFileSync(OUTPUT_JSON, JSON.stringify(photoList, null, 2));
 
   console.log('書き出し成功！');
